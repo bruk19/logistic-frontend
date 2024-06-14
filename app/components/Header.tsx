5453d788a156688346982cfe8dc4474c468d9a9f
Bruk Teshome <brukteshome21@gmail.com>
2024-06-14 17:10:44 +0300

    Wallet: display wallet address on the screnn

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,15 +1,52 @@
 'use client';
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { getWeb3, setUpWeb3 } from '../web3';
+import { ethers } from 'ethers';
+import { abi, contractAddress } from '../constants/logestics';
 
 function Header() {
   const [showMenu, setShowMenu] = useState<boolean>(false);
+  const [walletAddress, setWalletAddress] = useState<string | null>(null);
+  const [contract, setContract] = useState<ethers.Contract | undefined>(
+    undefined
+  );
+
+  useEffect(() => {
+    async function initialize() {
+      await setUpWeb3();
+      const web3Instance = getWeb3();
+      console.log(web3Instance);
+
+      const signer = await web3Instance.getSigner();
+      console.log(signer);
+
+      const contractInstance = new ethers.Contract(
+        contractAddress,
+        abi,
+        signer
+      );
+      setContract(contractInstance);
+
+      const wallet = await signer.getAddress();
+      setWalletAddress(wallet);
+    }
+    initialize();
+  }, []);
 
   const toggleMenu = () => {
     setShowMenu(!showMenu);
   };
 
   return (
-    <div className='w-full flex py-4 bg-zinc-600 px-20 justify justify-between'>
+    <div>
+      <div className='flex-end '>
+      {walletAddress? (
+        <p className=' text-lg font-bold mb-4 absolute mt-3 top-4 right-6 bg-white px-4 py-2 rounded-full shadow-sm border border-gray-300 '>Account: {walletAddress.slice(0,4)}...{walletAddress.slice(walletAddress.length-4)}</p>
+      ): (
+        <button>Connect Wallet</button>
+      )}
+    </div>
+      <div className='w-full flex py-4 bg-zinc-600 px-20 justify justify-between'>
       <div>
         <h1 className='text-slate-50 text-lg font-sans font-bold'>Logistic-Supply</h1>
       </div>
@@ -24,6 +61,7 @@ function Header() {
       <div className='visible sm:invisible'>
         <button className='bg-white p-3' onClick={toggleMenu}>{showMenu? 'X' : "Hamburger"}</button>
       </div>
+    </div>
     </div>
   )
 }
