f27b53b0c78eb11fa989b235802d0b55ae17749d
Bruk Teshome <brukteshome21@gmail.com>
2024-06-25 12:58:04 +0300

    MetaMask: conected to the supply chain manage page

--- app/supply/page.tsx
+++ app/supply/page.tsx
@@ -1,5 +1,27 @@
+import React, { useEffect, useState } from 'react'
+import { getWeb3, setUpWeb3 } from '../web3'
+import { ethers } from 'ethers'
+import { abi, contractAddress } from '../constants/logestics'
+
 function page() {
 
+  const [contract, setContract] = useState<ethers.Contract | undefined>(undefined)
+  const [wallletAddress, setWalletAddress] = useState<string | null>(null)
+
+  useEffect(() =>{
+    async function initialize () {
+      await setUpWeb3()
+      const web3Instance = getWeb3()
+      const signer = await web3Instance.getSigner();
+      const contractInstance = new ethers.Contract(contractAddress, abi, signer)
+
+      setContract(contractInstance)
+
+      const wallet = await signer.getAddress()
+      setWalletAddress(wallet)
+    }
+    initialize()
+  }, [])
   return (
     <div>page</div>
   )
