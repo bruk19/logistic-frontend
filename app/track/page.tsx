aaab41fb495c6c5f0698684077f3275d08f0fb42
Bruk Teshome <brukteshome21@gmail.com>
2024-06-26 10:51:16 +0300

    connect metamask: to the track page

--- app/track/page.tsx
+++ app/track/page.tsx
@@ -1,6 +1,24 @@
-import React from 'react'
+import { ethers } from 'ethers'
+import React, { useEffect, useState } from 'react'
+import { getWeb3, setUpWeb3 } from '../web3'
+import { abi, contractAddress } from '../constants/logestics'
 
 function page() {
+  const [contract, setContract] = useState<ethers.Contract | undefined>(undefined)
+  const [wallet, setWalletAddress] = useState<string | null>(null)
+
+  useEffect(() => {
+    async function initialize() {
+      await setUpWeb3();
+      const web3Instance = getWeb3();
+      const signer = await web3Instance.getSigner()
+      const contractInstance = new ethers.Contract(contractAddress, abi, signer)
+
+      setContract(contractInstance)
+      const wallet = await signer.getAddress()
+      setWalletAddress(wallet)
+    }
+  }, [])
   return (
     <div>page</div>
   )
