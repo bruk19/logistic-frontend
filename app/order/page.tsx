8d2d409f0fc2cc9a68f07a2333bf621b4b2d2dc7
Bruk Teshome <brukteshome21@gmail.com>
2024-06-28 14:45:13 +0300

    Displayed: error message on order product page

--- app/order/page.tsx
+++ app/order/page.tsx
@@ -129,6 +129,15 @@ function page() {
 
   return (
     <div className="m-10">
+      {errorMessage && (
+          <div
+            className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+            role="alert"
+          >
+            <strong className="font-bold">Error:</strong>
+            <span className="block sm:inline">{errorMessage}</span>
+          </div>
+        )}
       <h1 className="text-2xl font-bold mb-2">Add Product Order</h1>
       <div>
         <input
