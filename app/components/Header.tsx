7f2252fb18a7fac2bc2fdc7a6f269f4540c8a8ff
Bruk Teshome <brukteshome21@gmail.com>
2024-06-28 12:17:37 +0300

    Loading button: added when the page relaod

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -10,6 +10,7 @@ function Header() {
   const [contract, setContract] = useState<ethers.Contract | undefined>(
     undefined
   );
+  const[isLoading, setIsLoading] = useState<boolean>(true)
 
   useEffect(() => {
     async function initialize() {
@@ -29,13 +30,32 @@ function Header() {
 
       const wallet = await signer.getAddress();
       setWalletAddress(wallet);
+      setIsLoading(false);
     }
     initialize();
-  }, []);
+  }, [walletAddress]);
 
   const toggleMenu = () => {
     setShowMenu(!showMenu);
   };
+
+ const connectWallet = async() => {
+    setIsLoading(true);
+    try {
+      await setUpWeb3();
+      const web3Instance = getWeb3();
+      const signer = await web3Instance.getSigner();
+      const contractInstance = new ethers.Contract(contractAddress, abi, signer);
+      setContract(contractInstance);
+
+      const wallet = await signer.getAddress();
+      setWalletAddress(wallet);
+    } catch (error) {
+      console.log('Error connecting wallet: ', error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
 
   return (
     <div>
@@ -44,7 +64,7 @@ function Header() {
         <h1 className='text-slate-50 text-lg font-sans font-bold'>Logistic-Supply</h1>
       </div>
       <div>
-        <ul className={`text-slate-50 gap-y-4 ${showMenu ? 'block' : 'hidden'} sm:flex sm:gap-x-10 sm:visible`}>
+        <ul className={`text-slate-50 relative left-40 gap-y-4 ${showMenu ? 'block' : 'hidden'} sm:flex sm:gap-x-10 sm:visible`}>
           <li><a href="/register"> Register</a></li>
           <li><a href="/order"> Order Product</a></li>
           <li><a href="/supply"> Control Supply Chain</a></li>
@@ -55,14 +75,35 @@ function Header() {
         <button className='bg-white p-3' onClick={toggleMenu}>{showMenu? 'X' : "Hamburger"}</button>
       </div>
     </div>
-    <div className='flex-end '>
-      {walletAddress? (
-        <p className=' text-lg font-bold mb-4 absolute mt-3 right-6 bg-white px-4 py-2 rounded-full shadow-sm border border-gray-300 '>Account: {walletAddress.slice(0,4)}...{walletAddress.slice(walletAddress.length-4)}</p>
-      ): (
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        >Connect Wallet</button>
-      )}
-    </div>
+   <div className='flex-end '>
+        {walletAddress ? (
+          <p className=' text-lg font-bold mb-4 absolute mt-3 right-6 bg-white px-4 py-2 rounded-full shadow-sm border border-gray-300 '>
+            Account: {walletAddress.slice(0, 4)}...{walletAddress.slice(walletAddress.length - 4)}
+          </p>
+        ) : (
+          <button
+            className={`bg-blue-500 hover:bg-blue-700 text-white font-bold absolute mt-3 right-6 py-2 px-4 rounded ${isLoading ? 'opacity-50 cursor-not-allowed' : ''}`}
+            onClick={connectWallet}
+            disabled={isLoading}
+          >
+            {isLoading ? (
+              <div className="flex items-center justify-center ">
+                <svg className="animate-spin h-5 w-5 text-white" viewBox="0 0 24 24">
+                  <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                  <path
+                    className="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                  ></path>
+                </svg>
+                <span className="ml-2">Loading...</span>
+              </div>
+            ) : (
+              'Connect Wallet'
+            )}
+          </button>
+        )}
+      </div>
     </div>
   )
 }
